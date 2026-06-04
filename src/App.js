83a2cb6b2dac6cb8abfa9d0671759db5b47a821b
呂侑芸 <C113156201@nkust.edu.tw>
2026-06-04 19:18:31 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink, Navigate, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, NavLink, Navigate, Link } from 'react-router-dom';
 import Rules from './pages/Rules';
 import Characters from './pages/Characters';
 import Task from './pages/Task';
@@ -121,7 +121,7 @@ function App() {
         {/* 手機版頂部工具列 */}
         <header className="mobile-header">
           <div className="mobile-logo">
-            <img src="/logo.png" alt="Logo" />
+            <img src="./logo.png" alt="Logo" />
           </div>
           <button className="menu-toggle" onClick={() => setIsMenuOpen(!isMenuOpen)}>
             {isMenuOpen ? '✕' : '☰'}

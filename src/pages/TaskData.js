93f1be642ccaa6f9de433aacb7b97038d5b1c87b
“Yuzuat“ <clairelu2005@gmail.com>
2026-06-04 23:00:08 +0800

    修復圖片路徑

--- src/pages/TaskData.js
+++ src/pages/TaskData.js
@@ -1,15 +1,15 @@
 // TaskData.js
 export const taskCardsData = [
-  { id: 1, name: '特工檔案 01', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_1-B.png' },
-  { id: 2, name: '特工檔案 02', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_2-B.png' },
-  { id: 3, name: '特工檔案 03', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_3-B.png' },
-  { id: 4, name: '特工檔案 04', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_4-B.png' },
-  { id: 5, name: '特工檔案 05', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_5-B.png' },
-  { id: 6, name: '特工檔案 06', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_6-B.png' },
-  { id: 7, name: '特工檔案 07', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_7-B.png' },
-  { id: 8, name: '特工檔案 08', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_8-B.png' },
-  { id: 9, name: '特工檔案 09', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_9-B.png' },
-  { id: 10, name: '特工檔案 10', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_10-B.png' },
-  { id: 11, name: '特工檔案 11', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_11-B.png' },
-  { id: 12, name: '特工檔案 12', imgFront: '/pictures/task/t_1-A-01.png', imgBack: '/pictures/task/t_12-B.png' }
+  { id: 1, name: '特工檔案 01', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_1-B.png') },
+  { id: 2, name: '特工檔案 02', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_2-B.png') },
+  { id: 3, name: '特工檔案 03', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_3-B.png') },
+  { id: 4, name: '特工檔案 04', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_4-B.png') },
+  { id: 5, name: '特工檔案 05', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_5-B.png') },
+  { id: 6, name: '特工檔案 06', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_6-B.png') },
+  { id: 7, name: '特工檔案 07', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_7-B.png') },
+  { id: 8, name: '特工檔案 08', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_8-B.png') },
+  { id: 9, name: '特工檔案 09', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_9-B.png') },
+  { id: 10, name: '特工檔案 10', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_10-B.png') },
+  { id: 11, name: '特工檔案 11', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_11-B.png') },
+  { id: 12, name: '特工檔案 12', imgFront: require('../pictures/task/t_1-A-01.png'), imgBack: require('../pictures/task/t_12-B.png') }
 ];
